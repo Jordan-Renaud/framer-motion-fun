7944fd0cfca7b04cd8e9b11ada832f3bda744727
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-10-26 10:25:09 +0100

    add footer with animation

--- src/App.js
+++ src/App.js
@@ -5,58 +5,100 @@ import "./App.css";
 
 /* cards are displayed bottom to top */
 
+const icons = [
+  { id: "fa-facebook-square", link: "https://www.facebook.com/jordan.renaud3" },
+  {
+    id: "fa-instagram-square",
+    link: "https://www.instagram.com/jordan__renaud/",
+  },
+  {
+    id: "fa-twitter-square",
+    link: "https://twitter.com/jordan_renaud",
+  },
+  {
+    id: "fa-linkedin",
+    link: "https://www.linkedin.com/in/jordan-renaud-064a8a167/",
+  },
+  {
+    id: "fa-github-square",
+    link: "https://github.com/Jordan-Renaud",
+  },
+];
+
 function App() {
-  const [deck, setDeck] = useState(["bottom", "middle", "top"]);
-  const [flipped, setFlipped] = useState([]);
-
-  function addToDeck() {
-    if (flipped.length > 0) {
-      setDeck([...deck, flipped.pop()]);
-    }
-  }
-
-  function flipCard() {
-    if (deck.length === 1) {
-      // reset deck to begining
-    } else {
-      setFlipped([...flipped, deck.pop()]);
-    }
-  }
-
   return (
     <div className="App">
-      <h1 className="title">Jordan Renaud Developer</h1>
       <motion.div
-        className="card-container"
-        initial={{ scale: 0.8, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{ ease: "easeIn", duration: 1 }}
+        className="grid-container"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.2 }}
       >
-        <AnimateSharedLayout type="crossfade">
-          <div>
-            {flipped.map((text) => (
-              <Card
-                deckSide={"back"}
-                content={text}
-                handleCardState={addToDeck}
-                key={text}
-              />
-            ))}
-          </div>
-          <div>
-            {deck.map((text) => (
-              <Card
-                deckSide={"front"}
-                content={text}
-                handleCardState={flipCard}
-                key={text}
-              />
-            ))}
-          </div>
-        </AnimateSharedLayout>
+        <h1 className="title">Jordan Renaud Developer</h1>
+
+        <footer>
+          {icons.map((icon) => (
+            <div className="link" key={icon.id}>
+              <a href={icon.link} target="_blank" rel="noreferrer">
+                <i className={`fab ${icon.id}`}></i>
+              </a>
+            </div>
+          ))}
+        </footer>
       </motion.div>
     </div>
   );
+  // // const [deck, setDeck] = useState(["bottom", "middle", "top"]);
+  // // const [flipped, setFlipped] = useState([]);
+
+  // // function addToDeck() {
+  // //   if (flipped.length > 0) {
+  // //     setDeck([...deck, flipped.pop()]);
+  // //   }
+  // // }
+
+  // // function flipCard() {
+  // //   if (deck.length === 1) {
+  // //     // reset deck to begining
+  // //   } else {
+  // //     setFlipped([...flipped, deck.pop()]);
+  // //   }
+  // // }
+
+  // return (
+  //   <div className="App">
+  //     <h1 className="title">Jordan Renaud Developer</h1>
+  //     <motion.div
+  //     // className="card-container"
+  //     // initial={{ scale: 0.8, opacity: 0 }}
+  //     // animate={{ scale: 1, opacity: 1 }}
+  //     // transition={{ ease: "easeIn", duration: 1 }}
+  //     >
+  //       <AnimateSharedLayout type="crossfade">
+  //         {/* <div>
+  //           {flipped.map((text) => (
+  //             <Card
+  //               deckSide={"back"}
+  //               content={text}
+  //               //handleCardState={addToDeck}
+  //               key={text}
+  //             />
+  //           ))}
+  //         </div> */}
+  //         <div>
+  //           {deck.map((text) => (
+  //             <Card
+  //               deckSide={"front"}
+  //               content={text}
+  //               //handleCardState={flipCard}
+  //               key={text}
+  //             />
+  //           ))}
+  //         </div>
+  //       </AnimateSharedLayout>
+  //     </motion.div>
+  //   </div>
+  // );
 }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -10,20 +10,25 @@ const variants = {
 export default function Card({ content, handleCardState, deckSide }) {
   return (
     <motion.div
-      layout
-      layoutId={content}
+      // layout
+      // layoutId={content}
       className={`Card ${deckSide}`}
       onClick={handleCardState}
-      whileHover={{
-        scale: 1.01,
-        transition: { duration: 0.3 },
-      }}
-      initial={deckSide === "front" ? "postClick" : "preClick"}
-      animate={deckSide === "back" ? "postClick" : "preClick"}
-      variants={variants}
-      transition={{ duration: 1.5 }}
+      // whileHover={{
+      //   scale: 1.01,
+      //   transition: { duration: 0.3 },
+      // }}
+      // initial={deckSide === "front" ? "postClick" : "preClick"}
+      // animate={deckSide === "back" ? "postClick" : "preClick"}
+      // variants={variants}
+      // transition={{ duration: 1.5 }}
     >
-      {deckSide === "back" ? <LaptopLady /> : <p>{content}</p>}
+      <div className="card-face card-side-front">
+        <p>{content}</p>
+      </div>
+      <div className="card-face card-side-back">
+        <LaptopLady />
+      </div>
     </motion.div>
   );
 }
