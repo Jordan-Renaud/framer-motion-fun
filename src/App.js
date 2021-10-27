2664ddf6583ae344d94cb3067e0e6fc36e1c06eb
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-10-27 10:16:08 +0100

    add content framework for cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { AnimateSharedLayout, motion } from "framer-motion";
 import Card from "./Card";
+import LaptopGirl from "./images/girl-with-laptop";
 import "./App.css";
 
 /* cards are displayed bottom to top */
@@ -25,6 +26,13 @@ const icons = [
   },
 ];
 
+const cardContent = [
+  {
+    front: { title: "", content: <LaptopGirl /> },
+    back: { title: "me", content: "gg" },
+  },
+];
+
 function App() {
   return (
     <div className="App">
@@ -35,18 +43,23 @@ function App() {
         transition={{ duration: 1.5 }}
       >
         <h1 className="title">Jordan Renaud Developer</h1>
+
         <motion.div
-          className="card-container"
           initial={{ height: 0 }}
           animate={{ height: 575 }}
           transition={{ delay: 2, duration: 3 }}
         >
           <motion.div
+            className="card-container"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 4, duration: 1.5 }}
           >
-            <Card />
+            <Card front={cardContent[0].front} back={cardContent[0].back} />
+            <p className="click-here">
+              click here!
+              <br />⮐
+            </p>
           </motion.div>
         </motion.div>
         <footer>
@@ -61,57 +74,6 @@ function App() {
       </motion.div>
     </div>
   );
-  // // const [deck, setDeck] = useState(["bottom", "middle", "top"]);
-  // // const [flipped, setFlipped] = useState([]);
-
-  // // function addToDeck() {
-  // //   if (flipped.length > 0) {
-  // //     setDeck([...deck, flipped.pop()]);
-  // //   }
-  // // }
-
-  // // function flipCard() {
-  // //   if (deck.length === 1) {
-  // //     // reset deck to begining
-  // //   } else {
-  // //     setFlipped([...flipped, deck.pop()]);
-  // //   }
-  // // }
-
-  // return (
-  //   <div className="App">
-  //     <h1 className="title">Jordan Renaud Developer</h1>
-  //     <motion.div
-  //     // className="card-container"
-  //     // initial={{ scale: 0.8, opacity: 0 }}
-  //     // animate={{ scale: 1, opacity: 1 }}
-  //     // transition={{ ease: "easeIn", duration: 1 }}
-  //     >
-  //       <AnimateSharedLayout type="crossfade">
-  //         {/* <div>
-  //           {flipped.map((text) => (
-  //             <Card
-  //               deckSide={"back"}
-  //               content={text}
-  //               //handleCardState={addToDeck}
-  //               key={text}
-  //             />
-  //           ))}
-  //         </div> */}
-  //         <div>
-  //           {deck.map((text) => (
-  //             <Card
-  //               deckSide={"front"}
-  //               content={text}
-  //               //handleCardState={flipCard}
-  //               key={text}
-  //             />
-  //           ))}
-  //         </div>
-  //       </AnimateSharedLayout>
-  //     </motion.div>
-  //   </div>
-  // );
 }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,7 @@
 import "./Card.css";
 import { useState } from "react";
-import { motion } from "framer-motion";
-import LaptopLady from "./images/girl-with-laptop.js";
 
-const variants = {
-  preClick: { rotateY: 0 },
-  postClick: { rotateY: 180 },
-};
-
-export default function Card() {
+export default function Card({ front, back }) {
   const [flipCard, setFlipCard] = useState("");
 
   function handleClick() {
@@ -16,30 +9,14 @@ export default function Card() {
   }
   return (
     <div className={`Card ${flipCard}`} onClick={handleClick}>
-      <div className="front-of-card">front</div>
-      <div className="back-of-card">back</div>
+      <div className="front-of-card">
+        <h2>{front.title}</h2>
+        {front.content}
+      </div>
+      <div className="back-of-card">
+        <h2>{back.title}</h2>
+        {back.content}
+      </div>
     </div>
-
-    // <motion.div
-    // layout
-    // layoutId={content}
-    // className={`Card ${deckSide}`}
-    // onClick={handleCardState}
-    // whileHover={{
-    //   scale: 1.01,
-    //   transition: { duration: 0.3 },
-    // }}
-    // initial={deckSide === "front" ? "postClick" : "preClick"}
-    // animate={deckSide === "back" ? "postClick" : "preClick"}
-    // variants={variants}
-    // transition={{ duration: 1.5 }}
-    // >
-    // <div className="card-face card-side-front">
-    //   <p>{content}</p>
-    // </div>
-    // <div className="card-face card-side-back">
-    //   <LaptopLady />
-    // </div>
-    // </motion.div>
   );
 }
