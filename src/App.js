b20b080381d9751a603f686283a2a9a8a4c6dfac
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-11-18 11:02:36 +0000

    create data file for footer and card data

--- src/App.js
+++ src/App.js
@@ -1,82 +1,10 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import Card from "./Card";
-import LaptopGirl from "./images/girl-with-laptop";
 import Arrow from "./images/arrow";
 import LinkIcon from "./LinkIcon";
-import easyRecipeCard from "./images/easy-recipes.png";
-import weatherAppCard from "./images/weather-app.png";
+import { icons, cardContentData } from "./data";
 import "./App.css";
-
-/* cards are displayed bottom to top */
-
-const icons = [
-  {
-    id: "fa-facebook-square",
-    link: "https://www.facebook.com/jordan.renaud3",
-    title: "Facebook",
-  },
-  {
-    id: "fa-instagram-square",
-    link: "https://www.instagram.com/jordan__renaud/",
-    title: "Instagram",
-  },
-  {
-    id: "fa-twitter-square",
-    link: "https://twitter.com/jordan_renaud",
-    title: "Twitter",
-  },
-  {
-    id: "fa-linkedin",
-    link: "https://www.linkedin.com/in/jordan-renaud-064a8a167/",
-    title: "LinkedIn",
-  },
-  {
-    id: "fa-github-square",
-    link: "https://github.com/Jordan-Renaud",
-    title: "GitHub",
-  },
-];
-
-//load back to front
-const cardContentData = [
-  {
-    front: { title: "Weather Project", content: <img src={weatherAppCard} /> },
-    back: {
-      title: "Weather Project",
-      content: [
-        "Using: Vanilla JavaScript, HTML, CSS",
-        "Design: Played with neumorphic design to give an apple-esk feel",
-        "Learnt: API usage, manipulating the DOM, first project finished.",
-      ],
-      link: "https://weather-app-jrenaud.netlify.app/",
-    },
-  },
-  {
-    front: {
-      title: "Component Based Recipes",
-      content: <img src={easyRecipeCard} />,
-      link: "https://easy-recipes-jr.netlify.app/",
-    },
-    back: {
-      title: "Component Based Recipes",
-      content: [
-        "Idea: Easily being able to swap out ingredients in a meal.",
-        "Using: React, HTMl, CSS",
-        "Design: Simple and clean, focusing on usability. No ads to distract the user.",
-        "Learnt: React Hooks, multiple page application, APIs, CSS grid.",
-      ],
-    },
-  },
-  {
-    front: { title: "", content: <LaptopGirl /> },
-    back: {
-      title: "me",
-      content: ["New Zealander Abroad in London, HTML, CSS, React, JavaScript"],
-      link: "#",
-    },
-  },
-];
 
 function App() {
   const [cardClickCount, setCardClickCount] = useState(0);
@@ -170,7 +98,16 @@ function App() {
             </p>
           </motion.div>
         </motion.div>
-        {/* <Arrow /> */}
+        {/* <motion.div
+          whileHover={{ scale: 1.2 }}
+          onHoverStart={(e) => {
+            console.log(e);
+          }}
+          onHoverEnd={(e) => {}}
+        >
+          <Arrow />
+        </motion.div> */}
+
         <footer>
           {icons.map((icon) => (
             <LinkIcon
---
+++ src/data.js
@@ -0,0 +1,71 @@
+import easyRecipeCard from "./images/easy-recipes.png";
+import weatherAppCard from "./images/weather-app.png";
+import LaptopGirl from "./images/girl-with-laptop";
+
+export const icons = [
+  {
+    id: "fa-facebook-square",
+    link: "https://www.facebook.com/jordan.renaud3",
+    title: "Facebook",
+  },
+  {
+    id: "fa-instagram-square",
+    link: "https://www.instagram.com/jordan__renaud/",
+    title: "Instagram",
+  },
+  {
+    id: "fa-twitter-square",
+    link: "https://twitter.com/jordan_renaud",
+    title: "Twitter",
+  },
+  {
+    id: "fa-linkedin",
+    link: "https://www.linkedin.com/in/jordan-renaud-064a8a167/",
+    title: "LinkedIn",
+  },
+  {
+    id: "fa-github-square",
+    link: "https://github.com/Jordan-Renaud",
+    title: "GitHub",
+  },
+];
+
+//load back to front
+export const cardContentData = [
+  {
+    front: { title: "Weather Project", content: <img src={weatherAppCard} /> },
+    back: {
+      title: "Weather Project",
+      content: [
+        "Using: Vanilla JavaScript, HTML, CSS",
+        "Design: Played with neumorphic design to give an apple-esk feel",
+        "Learnt: API usage, manipulating the DOM, first project finished.",
+      ],
+      link: "https://weather-app-jrenaud.netlify.app/",
+    },
+  },
+  {
+    front: {
+      title: "Component Based Recipes",
+      content: <img src={easyRecipeCard} />,
+      link: "https://easy-recipes-jr.netlify.app/",
+    },
+    back: {
+      title: "Component Based Recipes",
+      content: [
+        "Idea: Easily being able to swap out ingredients in a meal.",
+        "Using: React, HTMl, CSS",
+        "Design: Simple and clean, focusing on usability. No ads to distract the user.",
+        "Learnt: React Hooks, multiple page application, APIs, CSS grid.",
+      ],
+    },
+  },
+  {
+    front: { title: "", content: <LaptopGirl /> },
+    back: {
+      title: "me",
+      content: ["New Zealander Abroad in London, HTML, CSS, React, JavaScript"],
+      link: "#",
+    },
+  },
+];
