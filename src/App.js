5432f1562346819baa7c86ca4c1f2d0111f0bc07
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-11-19 10:58:22 +0000

    fix button position bug

--- src/App.js
+++ src/App.js
@@ -21,121 +21,119 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <motion.div
+      className="App"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+    >
+      <h1 className="title">Jordan Renaud Developer</h1>
+
       <motion.div
-        className="grid-container"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1.5 }}
+        initial={{ height: 0 }}
+        animate={{ height: 620 }}
+        transition={{ delay: 2, duration: 3 }}
       >
-        <h1 className="title">Jordan Renaud Developer</h1>
-
         <motion.div
-          initial={{ height: 0 }}
-          animate={{ height: 575 }}
-          transition={{ delay: 2, duration: 3 }}
+          className="card-container"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 4, duration: 1.5 }}
         >
-          <motion.div
-            className="card-container"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 4, duration: 1.5 }}
-          >
-            {cardContent.map((card, index) => (
-              <Card
-                addClickCount={addClick}
-                key={index}
-                front={card.front}
-                back={card.back}
-                placement={
-                  index === cardContent.length - 1 ? "top" : "underneath"
-                }
-              />
-            ))}
-
-            <form
-              className="contact-form"
-              name="contact"
-              method="POST"
-              data-netlify="true"
-            >
-              <h2 className="handwriting-heading">Contact Me</h2>
-              <p>
-                <label>
-                  <input
-                    className="text-input"
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                  />
-                </label>
-              </p>
-              <p>
-                <label>
-                  <input
-                    className="text-input"
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                  />
-                </label>
-              </p>
-              <p>
-                <label>
-                  <textarea
-                    className="text-input large"
-                    name="message"
-                    placeholder="Message"
-                  ></textarea>
-                </label>
-              </p>
-              <button className="send-form" type="submit">
-                Send
-              </button>
-            </form>
-
-            <p className="click-here">
-              click here!
-              <div className="small-arrow">
-                <Arrow />
-              </div>
-            </p>
-
-            <div className="next-card-button-container">
-              <motion.button
-                className="small-arrow"
-                whileHover={{ scale: 1.2 }}
-                onHoverStart={(e) => {
-                  console.log(e);
-                }}
-                onHoverEnd={(e) => {}}
-              >
-                <Arrow />
-              </motion.button>
-              <motion.button
-                className="small-arrow"
-                whileHover={{ scale: 1.2 }}
-                onHoverStart={(e) => {
-                  console.log(e);
-                }}
-                onHoverEnd={(e) => {}}
-              >
-                <RightArrow />
-              </motion.button>
-            </div>
-          </motion.div>
-        </motion.div>
-        <footer>
-          {icons.map((icon) => (
-            <LinkIcon
-              iconTitle={icon.title}
-              iconLink={icon.link}
-              iconId={icon.id}
+          {cardContent.map((card, index) => (
+            <Card
+              addClickCount={addClick}
+              key={index}
+              front={card.front}
+              back={card.back}
+              placement={
+                index === cardContent.length - 1 ? "top" : "underneath"
+              }
             />
           ))}
-        </footer>
+
+          <form
+            className="contact-form"
+            name="contact"
+            method="POST"
+            data-netlify="true"
+          >
+            <h2 className="handwriting-heading">Contact Me</h2>
+            <p>
+              <label>
+                <input
+                  className="text-input"
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                <input
+                  className="text-input"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                <textarea
+                  className="text-input large"
+                  name="message"
+                  placeholder="Message"
+                ></textarea>
+              </label>
+            </p>
+            <button className="send-form" type="submit">
+              Send
+            </button>
+          </form>
+
+          <p className="click-here">
+            click here!
+            <div className="small-arrow">
+              <Arrow />
+            </div>
+          </p>
+
+          <div className="next-card-button-container">
+            <motion.button
+              className="small-arrow"
+              whileHover={{ scale: 1.2 }}
+              onHoverStart={(e) => {
+                console.log(e);
+              }}
+              onHoverEnd={(e) => {}}
+            >
+              <Arrow />
+            </motion.button>
+            <motion.button
+              className="small-arrow"
+              whileHover={{ scale: 1.2 }}
+              onHoverStart={(e) => {
+                console.log(e);
+              }}
+              onHoverEnd={(e) => {}}
+            >
+              <RightArrow />
+            </motion.button>
+          </div>
+        </motion.div>
       </motion.div>
-    </div>
+      <footer>
+        {icons.map((icon) => (
+          <LinkIcon
+            iconTitle={icon.title}
+            iconLink={icon.link}
+            iconId={icon.id}
+          />
+        ))}
+      </footer>
+    </motion.div>
   );
 }
 
